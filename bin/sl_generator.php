de6afb155e0ea37762b4096ba3110f17e07d8139
prolic <saschaprolic@googlemail.com>
2012-07-14 19:31:45 +0200

    updated sl_generator and composer

--- bin/sl_generator.php
+++ bin/sl_generator.php
@@ -1,68 +1,38 @@
 <?php
 chdir(dirname(__DIR__));
 ini_set('display_errors', 1);
-define('APPLICATION_PATH_MD5', md5(getcwd()));
 
-include 'application/autoload.php';
+include 'vendor/autoload.php';
 
-$rules = array(
-    'help|h'          => 'Get usage message',
-    'environment|e=w' => 'Application environment (production, staging, development, testing)'
-);
+echo "\nreading config files...\n";
 
-try {
-    $opts = new Zend_Console_Getopt($rules);
-    $opts->parse();
-} catch (Zend_Console_Getopt_Exception $e) {
-    echo $e->getUsageMessage();
-    exit();
-}
+/*
+ * Sample implementation of merging 3 given config files
+ * change that for your needs
+ */
 
-if ($env = $opts->getOption('e')) {
-    echo "\nreading config files...\n";
-    define('APPLICATION_ENV', $env);
+/*
+$alias = new Zend\Config\Config(include 'application/configs/di/alias.php', true);
 
-    $alias = new Zend\Config\Config(include 'application/configs/di/alias.php', true);
+$zend = new Zend\Config\Config(include 'application/configs/di/zend.php', true);
 
-    $zend = new Zend\Config\Config(include 'application/configs/di/zend.php', true);
+$humus = new Zend\Config\Config(include 'application/configs/di/humus.php', true);
 
-    $humus = new Zend\Config\Config(include 'application/configs/di/humus.php', true);
+$config = $alias
+    ->merge($zend)
+    ->merge($humus)
+    ->toArray();
 
-    $gedmo = new Zend\Config\Config(include 'application/configs/di/gedmo.php', true);
+*/
 
-    $doctrine = new Zend\Config\Config(include 'application/configs/di/doctrine.php', true);
+echo "generating application context...\n";
+$generator = new \Humus\Di\ServiceLocator\Generator($config);
+$generator->setNamespace('Application');
+$generator->setContainerClass('Context');
+$file = $generator->getCodeGenerator();
+$file->setFilename('application/Context.php');
+$file->write();
 
-    $app = new Zend\Config\Config(include 'application/configs/di/application.php', true);
+echo "\ndone\n";
 
-    $controller = new Zend\Config\Config(include 'application/configs/di/controller.php', true);
-
-    $acl = new Zend\Config\Config(include 'application/configs/di/acl.php', true);
-
-    $settings = new Zend\Config\Config(include 'application/configs/di/settings.' . $env . '.php', true);
-
-    $config = $alias
-        ->merge($zend)
-        ->merge($humus)
-        ->merge($gedmo)
-        ->merge($doctrine)
-        ->merge($app)
-        ->merge($acl)
-        ->merge($controller)
-        ->merge($settings)  // must be last
-        ->toArray();
-
-    echo "generating application context...\n";
-    $generator = new \Humus\Di\ServiceLocator\Generator($config);
-    $generator->setNamespace('Application');
-    $generator->setContainerClass('Context');
-    $file = $generator->getCodeGenerator();
-    $file->setFilename('application/Context.php');
-    $file->write();
-
-    echo "\ndone\n";
-
-    exit();
-} else {
-    echo $opts->getUsageMessage();
-    exit();
-}
+exit();
